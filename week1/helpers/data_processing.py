06b50c4c0fd255c1ea30a827e1862cb84c3cf0c7
zhexinwu <1025839251@qq.com>
2024-01-29 12:41:32 +0100

    added data_processing.py for data cleaning and IO using DB

---
+++ week1/helpers/data_processing.py
@@ -0,0 +1,109 @@
+import numpy as np
+import pandas as pd
+import sqlite3
+
+from datetime import datetime as dt
+from typing import Union, Iterable
+
+
+def remove_ST_and_list_date(stocks: pd.DataFrame, min_list_duration=365) -> pd.DataFrame:
+    """
+    stocks: withh columns "name", "list_date"
+    """
+    today = pd.to_datetime(dt.today().strftime("%Y%m%d"))
+    mask_ST = stocks.name.str.contains("ST")
+    list_duration = (today - pd.to_datetime(stocks.list_date)) / pd.Timedelta(days=min_list_duration)
+    mask_long_duration = (list_duration > 1)
+    stocks_out = stocks[(~mask_ST) & mask_long_duration]
+
+    return stocks_out
+
+
+def remove_non_tradable_stocks(stocks: pd.DataFrame, trade_info: pd.DataFrame) -> pd.Series:
+    """
+    stocks: ts_code
+    trade_info: ts_code, vol
+
+    Returns
+    -------
+    A pd.Series of stock code
+    """
+    trade_info = trade_info[trade_info.vol > 0]
+    stocks_out = trade_info.merge(stocks, on="ts_code", how="inner")
+
+    return stocks_out.ts_code
+
+
+def read_table_names(db_filename: str):
+    conn = sqlite3.connect(db_filename)
+    cursor = conn.cursor()
+    stmt = """
+    SELECT name FROM sqlite_master 
+    WHERE type="table"
+    """
+    cursor.execute(stmt)
+    res = cursor.fetchall()
+    conn.close()
+
+    return res
+
+
+def remove_table(db_filename: str, table_name: str):
+    all_table_names = read_table_names(db_filename)
+    if (table_name,) not in all_table_names:
+        return
+    conn = sqlite3.connect(db_filename)
+    cursor = conn.cursor()
+    stmt = f"""
+    DROP TABLE {table_name}
+    """
+    cursor.execute(stmt)
+    conn.commit()
+    conn.close()
+
+
+def save_to_db(today: str, selected_stock_code: Iterable,
+               table_name: str, db_filename: str):
+    """
+    date, ts_code | status
+
+    status: 1
+    """
+    # if isinstance(today, str):
+    #     today = pd.to_datetime(today)
+
+    all_table_names = read_table_names(db_filename)
+    if (table_name,) in all_table_names:
+        df_prev = read_from_db(table_name, db_filename, parse_dates=False)
+        if today in df_prev.index.get_level_values(0).unique():
+            return
+
+    df = pd.DataFrame(columns=selected_stock_code)
+    # df.loc[today] = 0
+    df.loc[today, selected_stock_code] = 1
+    df = df.stack(level=-1).to_frame()
+    df.rename(columns={0: "status"}, inplace=True)
+
+    # df_prev = read_from_db(table_name, db_filename, parse_dates=False)
+    # if len(df_prev) > 0:
+    #     df = pd.concat([df_prev, df], axis=0)
+    # print(df)
+
+    conn = sqlite3.connect(db_filename)
+    df.to_sql(table_name, conn, if_exists="append", index=True, index_label=["date", "ts_code"])
+    conn.close()
+
+
+def read_from_db(table_name: str, db_filename: str, parse_dates=True):
+    conn = sqlite3.connect(db_filename)
+    stmt = f"""
+    SELECT *
+    FROM {table_name}
+    """
+    if parse_dates:
+        df = pd.read_sql(stmt, conn, index_col=["date", "ts_code"], parse_dates=["date"])
+    else:
+        df = pd.read_sql(stmt, conn, index_col=["date", "ts_code"])
+    conn.close()
+
+    return df
